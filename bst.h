5bdb568595724d9fdf17f4dac0d9d0ce92cacc9e
maixuanpham <51069707+maixuanpham@users.noreply.github.com>
2019-07-03 07:04:15 -0700

    Update bst.h

--- bst.h
+++ bst.h
@@ -9,15 +9,15 @@
 
 class SearchTree {				                        // a binary search tree
 public:
-    class Iterator;					                    // an iterator/position
-private:                                                // member data
-    BinaryTree T;                                       // the binary tree
-    int n;                                              // number of entries
+    class Iterator;					                // an iterator/position
+private:                                                		// member data
+    BinaryTree T;                                       		// the binary tree
+    int n;                                              		// number of entries
 protected:
     int count;
 public:
     SearchTree(): T(), n(0)
-    { count = 0;T.addRoot(); T.expandExternal(T.root()); }	// create the super root
+    { count = 0;T.addRoot(); T.expandExternal(T.root()); }		// create the super root
     int size() const; 					                // number of entries
     bool empty() const;					                // is the tree empty?
     int getCount()
@@ -25,34 +25,34 @@ public:
     Iterator find(const string& k)
     {
         count = 0;
-		TPos v = finder(k, root());				        // search from virtual root
-		if (!v.isExternal()) return Iterator(v);		// found it
-		else return end();					            // didn't find it
+		TPos v = finder(k, root());				 // search from virtual root
+		if (!v.isExternal()) return Iterator(v);		 // found it
+		else return end();					 // didn't find it
     }
-    Iterator insert(const string& k, const PopulationRecord& x)		        // insert (k,x)
+    Iterator insert(const string& k, const PopulationRecord& x)		 // insert (k,x)
         { count = 0;TPos v = inserter(k, x); return Iterator(v); }
     void erase(const string& k)
     {
         count = 0;
-		TPos v = finder(k, root());				        // search from virtual root
-		if (v.isExternal())					            // not found?
+		TPos v = finder(k, root());				 // search from virtual root
+		if (v.isExternal())					 // not found?
 		  throw NonexistentElement("Erase of nonexistent");
-		eraser(v);						                // remove it
+		eraser(v);						 // remove it
     }
-    void erase( Iterator& p)			            // remove entry at p
+    void erase( Iterator& p)			            		// remove entry at p
         { eraser(p.v); }
     Iterator begin()
     {
-		TPos v = root();					            // start at virtual root
-		while (!v.isExternal()) v = v.left();		    // find leftmost node
+		TPos v = root();					// start at virtual root
+		while (!v.isExternal()) v = v.left();		    	// find leftmost node
 		return Iterator(v.parent());
 	  }
-    Iterator end()					                    // iterator to end entry
-        { return Iterator(T.root()); }			        // return the super root
+    Iterator end()					                // iterator to end entry
+        { return Iterator(T.root()); }			        	// return the super root
 
-protected:						                        // local utilities
-    typedef BinaryTree::Position TPos;		    // position in the tree
-    TPos root() const    { return T.root().left(); }    // left child of super root
+protected:						                // local utilities
+    typedef BinaryTree::Position TPos;		    			// position in the tree
+    TPos root() const    { return T.root().left(); }    		// left child of super root
     TPos finder(const string& k, const TPos& v)
     {
         count++;
@@ -64,28 +64,28 @@ protected:						                        // local utilities
     TPos inserter(const string& k, const PopulationRecord& x)
     {
 		TPos v = finder(k, root());				        // search from virtual root
-		while (!v.isExternal())				            // key already exists?
+		while (!v.isExternal())				            	// key already exists?
 		  v = finder(k, v.right());				        // look further
-		T.expandExternal(v);				            // add new internal node
-		(*v).setKey(k); (*v).setValue(x);			    // set entry
-		n++;						                    // one more entry
-		return v;						                // return insert position
+		T.expandExternal(v);				            	// add new internal node
+		(*v).setKey(k); (*v).setValue(x);			    	// set entry
+		n++;						                // one more entry
+		return v;						        // return insert position
     }
     TPos eraser(TPos& v) {
 		TPos w;
-		if (v.left().isExternal()) w = v.left();		// remove from left
-		else if (v.right().isExternal()) w = v.right();	// remove from right
-		else {						// both internal?
-		  w = v.right();					// go to right subtree
-		  do { w = w.left(); } while (!w.isExternal());	// get leftmost node
+		if (v.left().isExternal()) w = v.left();			// remove from left
+		else if (v.right().isExternal()) w = v.right();			// remove from right
+		else {								// both internal?
+		  w = v.right();						// go to right subtree
+		  do { w = w.left(); } while (!w.isExternal());			// get leftmost node
 		  TPos u = w.parent();
-		  (*v).setKey((*u).key()); (*v).setValue((*u).value());	// copy w's parent to v
+		  (*v).setKey((*u).key()); (*v).setValue((*u).value());		// copy w's parent to v
 		}
-		n--;						// one less entry
-		return T.removeAboveExternal(w);			// remove w and parent
+		n--;								// one less entry
+		return T.removeAboveExternal(w);				// remove w and parent
     }
 
-    TPos restructure(const TPos& v) 			    // restructure
+    TPos restructure(const TPos& v) 			    			// restructure
     {
         TPos x, y, z, a, b, c, t0, t1, t2, t3;
         
@@ -171,31 +171,31 @@ protected:						                        // local utilities
 
 public:
     // ...insert Iterator class declaration here
-    class Iterator {	                      		// an iterator/position
+    class Iterator {	                      				// an iterator/position
     private:
       TPos v;						                // which entry
     public:
-      Iterator(const TPos& vv) : v(vv) { }		    // constructor
-      //const AVLEntry& operator*() const { return *v.v; }	    // get entry (read only)
+      Iterator(const TPos& vv) : v(vv) { }		    		// constructor
+      //const AVLEntry& operator*() const { return *v.v; }	    	// get entry (read only)
       AVLEntry& operator*() { return *v; }			        // get entry (read/write)
-      bool operator==(const Iterator& p) const		// are iterators equal?
+      bool operator==(const Iterator& p) const				// are iterators equal?
         { return v.v == p.v.v; }
       Iterator& operator++(){
 		TPos w = v.right();
 		if (!w.isExternal()) {				        // have right subtree?
-		  do { v = w; w = w.left(); }			    // move down left chain
+		  do { v = w; w = w.left(); }			    	// move down left chain
 		  while (!w.isExternal());
 		}
 		else {
-		  w = v.parent();					        // get parent
-		  while (v.v == w.right().v)				    // move up right chain
+		  w = v.parent();					// get parent
+		  while (v.v == w.right().v)				// move up right chain
 			{ v = w; w = w.parent(); }
-		  v = w;						            // and first link to left
+		  v = w;						// and first link to left
 		}
 		return *this;
 	  }
 
-      friend class SearchTree;				        // give search tree access
+      friend class SearchTree;				        	// give search tree access
     };
 };
 
